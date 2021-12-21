1cce305583beb88a360296fdacbc4a6454ce80ff
yuri0105 <87520906+yuri0105@users.noreply.github.com>
2021-12-22 07:21:06 +1000

    final_12_22

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,13 +1,13 @@
 export const getArtUrl = (tokenId) => {
     var artUrl = '';
     if (tokenId >= 1 && tokenId <= 2000) {
-        artUrl = 'https://gateway.pinata.cloud/ipfs/QmRjf2bZqqEJxhoPPeNqg4wMmdEd7NcfsDNNYdsjLLt23H/Pattern_' + tokenId + '.png';
+        artUrl = 'https://areanroyale.mypinata.cloud/ipfs/QmRjf2bZqqEJxhoPPeNqg4wMmdEd7NcfsDNNYdsjLLt23H/Pattern_' + tokenId + '.png';
     } else if (tokenId <= 4000) {
-        artUrl = 'https://gateway.pinata.cloud/ipfs/QmPnizdA3Doi6xU2Lv2FCt1KCratstJePjpPySxwcpMfEZ/Pattern_' + tokenId + '.png';
+        artUrl = 'https://areanroyale.mypinata.cloud/ipfs/QmPnizdA3Doi6xU2Lv2FCt1KCratstJePjpPySxwcpMfEZ/Pattern_' + tokenId + '.png';
     } else if (tokenId <= 6000) {
-        artUrl = 'https://gateway.pinata.cloud/ipfs/QmS9E1B74RvrA6oxpN76ixoEieuJmdNKsasZP8p6x9LJCz/Pattern_' + tokenId + '.png';
+        artUrl = 'https://areanroyale.mypinata.cloud/ipfs/QmS9E1B74RvrA6oxpN76ixoEieuJmdNKsasZP8p6x9LJCz/Pattern_' + tokenId + '.png';
     } else if (tokenId <= 10000) {
-        artUrl = 'https://gateway.pinata.cloud/ipfs/QmRMfv6a72PmDz2Zy5eAWz3B2HeDdGNLRupHQm6vw4sU3g/Pattern_' + tokenId + '.png';
+        artUrl = 'https://areanroyale.mypinata.cloud/ipfs/QmRMfv6a72PmDz2Zy5eAWz3B2HeDdGNLRupHQm6vw4sU3g/Pattern_' + tokenId + '.png';
     }
     return artUrl;
 }
